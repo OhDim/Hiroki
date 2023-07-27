4d64e5fa7819c183be3010f2e6ab43c06aa1d09e
Mathe <Mathe@HOME>
2023-07-27 19:04:51 -0300

    Adicionar arquivos de projeto.

---
+++ Hiroki/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Hiroki.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Hiroki.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Transferencias()
+        {
+            return View();
+        }
+
+        public IActionResult Pagamentos()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
